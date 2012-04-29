40a040b2673e068dfb20e6127fc13739a012f2c7
Tomas Jukin <tomas@jukin.cz>
2012-04-29 13:25:38 +0200

    Generated git repository statistics

--- generate_stats_with_video.sh
+++ generate_stats_with_video.sh
@@ -43,5 +43,5 @@ find . -type d -maxdepth 1 -exec bash -c "cd '{}' && git add -A && git commit -m
 
 # Copy all videos to one folder
 mkdir -p videos
-rm -f videos/*
+rm -fR videos/*
 cp -f */$VIDEO_OUTPUT_PATH/*.mp4 "./videos/"
