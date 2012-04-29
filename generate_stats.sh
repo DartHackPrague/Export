fbafe52ff16f00419ef58dd18fd66ad1f2955d88
Tomas Jukin <tomas@jukin.cz>
2012-04-29 08:31:11 +0200

    Added videos folder

--- generate_stats.sh
+++ generate_stats.sh
@@ -18,10 +18,10 @@ find . -type d -mindepth 1 -maxdepth 1 -exec bash -c "cd '{}' && rm -Rf ./$STATS
 find . -type d -mindepth 1 -maxdepth 1 -exec bash -c "cd '{}' && $GITSTATS_SCRIPT_PATH . ./$STATS_OUTPUT_PATH" \;
 
 # Add, Commit and Push generated stats
-find . -type d -maxdepth 1 -exec bash -c "cd '{}' && git add -A && git commit -m 'Generated git repository statistics' && git pull origin master && git push origin master" \;
+#find . -type d -maxdepth 1 -exec bash -c "cd '{}' && git add -A && git commit -m 'Generated git repository statistics' && git pull origin master && git push origin master" \;
 
 # Update Submodules
-git submodule update
+#git submodule update
 
 # Generate TOP Night Coder results
 rm -f "top_night_coder.txt"
@@ -31,10 +31,14 @@ find . -type d -mindepth 1 -maxdepth 1 -exec bash -c "cd '{}/$STATS_OUTPUT_PATH'
 cat "top_night_coder_results.txt"
 
 # Clean stats
-find . -type d -mindepth 1 -maxdepth 1 -exec bash -c "cd '{}' && rm -Rf ./$VIDEO_OUTPUT_PATH" \;
+#find . -type d -mindepth 1 -maxdepth 1 -exec bash -c "cd '{}' && rm -Rf ./$VIDEO_OUTPUT_PATH" \;
 
 # Generate gource
-find . -type d -mindepth 1 -maxdepth 1 -exec bash -c "cd '{}' && $GOURCE_SCRIPT_PATH $VIDEO_OUTPUT_PATH" \;
+#find . -type d -mindepth 1 -maxdepth 1 -exec bash -c "cd '{}' && $GOURCE_SCRIPT_PATH $VIDEO_OUTPUT_PATH" \;
 
-# Add, Commit and Push generated stats
-find . -type d -maxdepth 1 -exec bash -c "cd '{}' && git add -A && git commit -m 'Generated git repository visualization' && git pull origin master && git push origin master" \;
+# Add, Commit and Push generated videos
+#find . -type d -maxdepth 1 -exec bash -c "cd '{}' && git add -A && git commit -m 'Generated git repository visualization' && git pull origin master && git push origin master" \;
+
+# Copy all videos to one folder
+#mkdir -p videos
+#cp -f "*/$VIDEO_OUTPUT_PATH/*.mp4" "./videos/"
